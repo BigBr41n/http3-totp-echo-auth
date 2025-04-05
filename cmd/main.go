8014157760add6dba624f6074cad7ac774ea3fb0
rahim <0xr4him@archlinux.localdomain>
2025-04-05 20:58:55 +0100

    refactor : inject the DBpool and the user repo and tesing the service of signup

--- cmd/main.go
+++ cmd/main.go
@@ -2,9 +2,13 @@ package main
 
 import (
 	"log"
+	"time"
 
 	"github.com/BigBr41n/echoAuth/db"
 	"github.com/BigBr41n/echoAuth/internal/logger"
+	"github.com/BigBr41n/echoAuth/models"
+	"github.com/BigBr41n/echoAuth/services"
+	"github.com/google/uuid"
 
 	"github.com/BigBr41n/echoAuth/config"
 )
@@ -18,6 +22,25 @@ func main() {
 	logger.InitLogger(logger.DefaultConfig())
 
 	db.ConnectDB()
+
+	userRepo := models.NewUserRepo(db.DBPool)
+	userService := services.NewUserService(userRepo)
+
+	userID, err := userService.SignUp(&models.User{
+		ID:        uuid.UUID{},
+		Username:  "R4him",
+		Email:     "testing@gmaol.co",
+		Password:  "pass#--*--$7R0NG",
+		Role:      "client",
+		CreatedAt: time.Now(),
+		UpdatedAt: time.Now(),
+	})
+
+	if err != nil {
+		logger.Error(err)
+	}
+
+	logger.Info(userID)
 
 	logger.Info("Application started")
 	logger.WithField("user_id", 123).Info("User logged in")
