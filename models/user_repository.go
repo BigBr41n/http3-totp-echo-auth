4d29a5c2d322a96b9c0f1533927edb25faa1836d
rahim <0xr4him@archlinux.localdomain>
2025-04-05 20:56:05 +0100

    refactor : using interface dependency injection as a best practice for easier testing3

--- models/user_repository.go
+++ models/user_repository.go
@@ -2,21 +2,40 @@ package models
 
 import (
 	"context"
+	"errors"
 	"fmt"
 	"log"
 
-	"github.com/BigBr41n/echoAuth/db"
+	"github.com/BigBr41n/echoAuth/internal/logger"
+	"github.com/google/uuid"
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgx/v5/pgxpool"
 )
 
+type UserRepository struct {
+	Db *pgxpool.Pool
+}
+
+type UserRepoI interface {
+	CreateUser(user *User) error
+	GetUserByID(id uuid.UUID) (*User, error)
+	GetUserByEmail(email string) (*User, error)
+}
+
+func NewUserRepo(db *pgxpool.Pool) UserRepoI {
+	return &UserRepository{
+		Db: db,
+	}
+}
+
 // CreateUser creates a new user in the database
-func CreateUser(user *User) error {
+func (usr *UserRepository) CreateUser(user *User) error {
 	// Prepare the SQL query to insert a new user
 	query := `INSERT INTO users (username, email, password, created_at, updated_at)
 			  VALUES ($1, $2, $3, NOW(), NOW()) RETURNING id`
 
 	// Execute the query
-	err := db.DBPool.QueryRow(context.Background(), query, user.Username, user.Email, user.Password).Scan(&user.ID)
+	err := usr.Db.QueryRow(context.Background(), query, user.Username, user.Email, user.Password).Scan(&user.ID)
 	if err != nil {
 		if err == pgx.ErrNoRows {
 			return fmt.Errorf("no rows affected while creating user")
@@ -30,7 +49,7 @@ func CreateUser(user *User) error {
 }
 
 // GetUserByID retrieves a user by ID from the database
-func GetUserByID(id int) (*User, error) {
+func (usr *UserRepository) GetUserByID(id uuid.UUID) (*User, error) {
 	// Prepare the SQL query to get a user by ID
 	query := `SELECT id, username, email, password, created_at, updated_at FROM users WHERE id = $1`
 
@@ -38,7 +57,7 @@ func GetUserByID(id int) (*User, error) {
 	var user User
 
 	// Execute the query
-	err := db.DBPool.QueryRow(context.Background(), query, id).Scan(&user.ID, &user.Username, &user.Email, &user.Password, &user.CreatedAt, &user.UpdatedAt)
+	err := usr.Db.QueryRow(context.Background(), query, id).Scan(&user.ID, &user.Username, &user.Email, &user.Password, &user.CreatedAt, &user.UpdatedAt)
 	if err != nil {
 		if err == pgx.ErrNoRows {
 			return nil, fmt.Errorf("no user found with ID %d", id)
@@ -48,5 +67,25 @@ func GetUserByID(id int) (*User, error) {
 	}
 
 	log.Println("User retrieved successfully:", user.Username)
+	return &user, nil
+}
+
+// GetUserByID retrieves a user by email from the database
+func (usr *UserRepository) GetUserByEmail(email string) (*User, error) {
+	query := `SELECT id, username, email, password, created_at, updated_at FROM users WHERE id = $1`
+
+	var user User
+
+	// Execute the query
+	err := usr.Db.QueryRow(context.Background(), query, email).Scan(&user.ID, &user.Username, &user.Email, &user.Password, &user.CreatedAt, &user.UpdatedAt)
+	if err != nil {
+		if err == pgx.ErrNoRows {
+			logger.Error(err)
+			return nil, fmt.Errorf("no user found with email %s", email)
+		}
+		logger.Error(err)
+		return nil, errors.New("internal server error")
+	}
+
 	return &user, nil
 }
