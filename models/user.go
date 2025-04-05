e181447add5884bcfc2c09ee98efce378499b1bd
rahim <0xr4him@archlinux.localdomain>
2025-04-05 20:55:05 +0100

    refactor : adding UUID as ID

--- models/user.go
+++ models/user.go
@@ -2,13 +2,16 @@ package models
 
 import (
 	"time"
+
+	"github.com/google/uuid"
 )
 
 // User represents a user in the system
 type User struct {
-	ID        int       `json:"id"`
+	ID        uuid.UUID `json:"id"`
 	Username  string    `json:"username"`
 	Email     string    `json:"email"`
+	Role      string    `json:"role"`
 	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at"`
