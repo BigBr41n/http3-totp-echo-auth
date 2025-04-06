7747e0c99c28b3c2368e9124df08635a835a6946
rahim <0xr4him@archlinux.localdomain>
2025-04-06 01:05:06 +0100

    setup : setuping sqlc with schema and the queries

---
+++ queries/user_queries.sql
@@ -0,0 +1,15 @@
+-- name: CreateUser :one
+INSERT INTO users (id, username, email, password, role, created_at, updated_at)
+VALUES ($1, $2, $3, $4, $5, NOW(), NOW())
+RETURNING id, username, email, password, role, created_at, updated_at;
+
+-- name: GetUserByEmail :one
+SELECT id, username, email, password, role, created_at, updated_at
+FROM users
+WHERE email = $1;
+
+-- name: GetUserByID :one
+SELECT id, username, email, password, role, created_at, updated_at
+FROM users
+WHERE id = $1;
+
