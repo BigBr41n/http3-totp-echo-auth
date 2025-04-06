6a384d50940dff0317228249a81b806f42ad5840
rahim <0xr4him@archlinux.localdomain>
2025-04-06 01:07:51 +0100

    refactor : rewriting the services using the queries instead of userRepositort

--- services/user.service.go
+++ services/user.service.go
@@ -1,28 +1,29 @@
 package services
 
 import (
+	"context"
 	"fmt"
 	"time"
 
+	"github.com/BigBr41n/echoAuth/db/sqlc"
 	"github.com/BigBr41n/echoAuth/internal/logger"
-	"github.com/BigBr41n/echoAuth/models"
 	"github.com/BigBr41n/echoAuth/utils/jwtImpl"
 	"github.com/golang-jwt/jwt/v5"
-	"github.com/google/uuid"
+	"github.com/jackc/pgx/v5/pgtype"
 )
 
 type UserServiceI interface {
-	SignUp(userData *models.User) (uuid.UUID, error)
+	SignUp(userData *sqlc.CreateUserParams) (pgtype.UUID, error)
 	Login(creds Credentials) (string, string, error)
 }
 
 type UserService struct {
-	userRepo models.UserRepoI
+	queries *sqlc.Queries
 }
 
-func NewUserService(userRepo models.UserRepoI) UserServiceI {
+func NewUserService(qrs *sqlc.Queries) UserServiceI {
 	return &UserService{
-		userRepo,
+		queries: qrs,
 	}
 }
 
@@ -31,24 +32,25 @@ type Credentials struct {
 	Password string
 }
 
-func (usr *UserService) SignUp(creds *models.User) (uuid.UUID, error) {
-	err := usr.userRepo.CreateUser(creds)
+func (usr *UserService) SignUp(userData *sqlc.CreateUserParams) (pgtype.UUID, error) {
+	user, err := usr.queries.CreateUser(context.Background(), *userData)
 	if err != nil {
 		logger.Error(err)
-		return uuid.Nil, fmt.Errorf("failed to create user")
+		return pgtype.UUID{}, fmt.Errorf("failed to create user")
 	}
-
-	return creds.ID, nil
+	return user.ID, nil
 }
 
 func (usr *UserService) Login(creds Credentials) (string, string, error) {
 
-	user, err := usr.userRepo.GetUserByEmail(creds.Email)
+	user, err := usr.queries.GetUserByEmail(context.Background(), creds.Email)
 	if err != nil {
+		logger.Error(err)
 		return "", "", err
 	}
 
 	if user.Password != creds.Password {
+		logger.Error(err)
 		return "", "", fmt.Errorf("invalid credentials")
 	}
 
