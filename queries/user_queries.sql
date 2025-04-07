3c43f696050cddcce067fd5acc4fa37091b4ef0e
rahim <0xr4him@archlinux.localdomain>
2025-04-07 11:14:21 +0100

    refactor : removing the uuid, instead it's auto generated by PostgerSQL

--- queries/user_queries.sql
+++ queries/user_queries.sql
@@ -1,6 +1,6 @@
 -- name: CreateUser :one
-INSERT INTO users (id, username, email, password, role, created_at, updated_at)
-VALUES ($1, $2, $3, $4, $5, NOW(), NOW())
+INSERT INTO users (username, email, password, role, created_at, updated_at)
+VALUES ($1, $2, $3, $4, NOW(), NOW())
 RETURNING id, username, email, password, role, created_at, updated_at;
 
 -- name: GetUserByEmail :one
