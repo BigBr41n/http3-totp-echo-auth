d437d9d2283b92233753b1f3baa8dd448430d07c
rahim <0xr4him@archlinux.localdomain>
2025-04-09 23:44:07 +0100

    refactor : using zap logger structured logging

--- services/auth_service.go
+++ services/auth_service.go
@@ -9,8 +9,10 @@ import (
 	"github.com/BigBr41n/echoAuth/db/sqlc"
 	"github.com/BigBr41n/echoAuth/internal/logger"
 	"github.com/BigBr41n/echoAuth/utils/jwtImpl"
+	"github.com/BigBr41n/echoAuth/utils/validator"
 	"github.com/golang-jwt/jwt/v5"
 	"github.com/jackc/pgx/v5/pgtype"
+	"go.uber.org/zap"
 )
 
 type AuthServiceI interface {
@@ -35,11 +37,28 @@ type Credentials struct {
 }
 
 func (usr *AuthService) SignUp(userData *dtos.CreateUserDTO) (pgtype.UUID, error) {
+
+	// validate user input
+	err := validator.Validate(userData)
+	if err != nil {
+		logger.Error("failed to create user",
+			zap.String("context", "DTO validation failed"),
+			zap.Error(err),
+		)
+		return pgtype.UUID{}, err
+	}
 	user, err := usr.queries.CreateUser(context.Background(), (sqlc.CreateUserParams)(*userData))
 	if err != nil {
-		logger.Error(err)
-		return pgtype.UUID{}, fmt.Errorf("failed to create user")
+		logger.Error("failed to create user",
+			zap.String("reason", err.Error()),
+			zap.Error(err),
+		)
+		return pgtype.UUID{}, err
 	}
+
+	logger.Error("new user created",
+		zap.String("userId", user.ID.String()),
+	)
 	return user.ID, nil
 }
 
@@ -47,12 +66,17 @@ func (usr *AuthService) Login(creds *Credentials) (string, string, error) {
 
 	user, err := usr.queries.GetUserByEmail(context.Background(), creds.Email)
 	if err != nil {
-		logger.Error(err)
+		logger.Error("failed to login",
+			zap.String("reason", err.Error()),
+			zap.Error(err),
+		)
 		return "", "", err
 	}
 
 	if user.Password != creds.Password {
-		logger.Error(err)
+		logger.Error("failed passwrod checking operation",
+			zap.String("user", user.ID.String()),
+		)
 		return "", "", fmt.Errorf("invalid credentials")
 	}
 
@@ -68,10 +92,16 @@ func (usr *AuthService) Login(creds *Credentials) (string, string, error) {
 	// generate the auth tokens (access & refresh)
 	accessToken, refreshToken, err := jwtImpl.GenerateToken(claims)
 	if err != nil {
-		logger.Error(err)
+		logger.Error("failed to login",
+			zap.String("reason", err.Error()),
+			zap.Error(err),
+		)
 		return "", "", err
 	}
 
+	logger.Error("User logged in",
+		zap.String("userId", user.ID.String()),
+	)
 	return accessToken, refreshToken, nil
 }
 
@@ -79,7 +109,10 @@ func (usr *AuthService) RefreshUserToken(refTok string, oldTok string) (string, 
 	newRefTok, err := jwtImpl.RefreshAccessToken(refTok, oldTok)
 
 	if err != nil {
-		logger.Error("refresh token generation failed", err)
+		logger.Error("failed to refresh the token",
+			zap.String("reason", err.Error()),
+			zap.Error(err),
+		)
 		return "", err
 	}
 
