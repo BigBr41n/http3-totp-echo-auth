a802ab22c6757051b0b89d9b46297af7eaa08287
rahim <0xr4him@archlinux.localdomain>
2025-04-09 23:43:36 +0100

    refactor: using the new registered validation

--- DTOs/user_dtos.go
+++ DTOs/user_dtos.go
@@ -3,7 +3,7 @@ package dtos
 type CreateUserDTO struct {
 	Username string `json:"username" validate:"required,min=4,max=20"`
 	Email    string `json:"email" validate:"required,email"`
-	Password string `json:"password" validate:"required,regexp=^.*(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[!@#$%^&*()_+]).*$"`
+	Password string `json:"password" validate:"required,pwd"`
 	Role     string `json:"role" validate:"required,oneof=client seller investor"`
 }
 
