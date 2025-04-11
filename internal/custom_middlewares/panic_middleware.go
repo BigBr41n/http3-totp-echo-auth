cb6350513f104229fe9c74684c6d78a166bf2ba3
rahim <0xr4him@archlinux.localdomain>
2025-04-11 21:55:23 +0100

    feat: panic midlware to send back internal server err in case any panic

---
+++ internal/custom_middlewares/panic_middleware.go
@@ -0,0 +1,40 @@
+package custommiddlewares
+
+import (
+	"net/http"
+	"os"
+	"runtime/debug"
+
+	dtos "github.com/BigBr41n/echoAuth/DTOs"
+	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
+)
+
+func RecoverWithJSON() echo.MiddlewareFunc {
+	return func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			defer func() {
+				if r := recover(); r != nil {
+					// Log the panic and stacktrace
+					zap.L().Error("PANIC recovered",
+						zap.Any("panic", r),
+						zap.String("stack", string(debug.Stack())))
+
+					errMsg := "Internal Server Error"
+					if os.Getenv("ECHO_AUTH_APP") != "prod" {
+						errMsg = r.(string)
+					}
+
+					_ = c.JSON(http.StatusInternalServerError, dtos.ErrResponse{
+						Status:  http.StatusInternalServerError,
+						Error:   errMsg,
+						Code:    "INTERNAL_ERROR",
+						Details: nil,
+					})
+				}
+			}()
+
+			return next(c)
+		}
+	}
+}
