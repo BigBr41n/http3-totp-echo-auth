d9dcc8cfa992c84411a6483d52173ddbdf093cd8
rahim <0xr4him@archlinux.localdomain>
2025-04-13 00:41:26 +0100

    refactor : making the error sender function more generic and refuse any error didn't send by the developer and return 500 err till the dev fix it

--- utils/response/response_sender.go
+++ utils/response/response_sender.go
@@ -1,31 +1,49 @@
 package response
 
 import (
+	"net/http"
 	"os"
 
 	dtos "github.com/BigBr41n/echoAuth/DTOs"
+	"github.com/BigBr41n/echoAuth/internal/logger"
 	"github.com/labstack/echo/v4"
+	"go.uber.org/zap"
 )
 
 // Success Response
-func ValResp(c echo.Context, status int, code string, message string, data interface{}) error {
-	return c.JSON(status, dtos.ValidResponse{
-		Status:  status,
-		Code:    code,
-		Message: message,
-		Data:    data,
+func ValResp(c echo.Context, resp *dtos.ValidResponse) error {
+	return c.JSON(resp.Status, dtos.ValidResponse{
+		Status:  resp.Status,
+		Code:    resp.Code,
+		Message: resp.Message,
+		Data:    resp.Data,
 	})
 }
 
 // Error Response
-func ErrResp(c echo.Context, status int, code string, errMsg string, details interface{}) error {
-	if (os.Getenv("ECHO_AUTH_APP") == "prod") && (status == 500) {
-		errMsg = "Internal Server Error"
+func ErrResp(c echo.Context, resp error) error {
+	var ApiError *dtos.ApiErr
+	var ok bool
+	// assert back the error if possible
+	if ApiError, ok = resp.(*dtos.ApiErr); !ok {
+		logger.Error("Unknown error", zap.Error(resp))
+
+		return c.JSON(http.StatusInternalServerError, dtos.ApiErr{
+			Status:  http.StatusInternalServerError,
+			Err:     resp.Error(),
+			Code:    "INTERNAL_ERROR",
+			Details: "Unkown error",
+		})
 	}
-	return c.JSON(status, dtos.ErrResponse{
-		Status:  status,
-		Code:    code,
-		Error:   errMsg,
-		Details: details,
+
+	if (os.Getenv("ECHO_AUTH_APP") == "prod") && (ApiError.Status == 500) {
+		ApiError.Err = "Internal Server Error"
+	}
+
+	return c.JSON(ApiError.Status, dtos.ApiErr{
+		Status:  ApiError.Status,
+		Code:    ApiError.Code,
+		Err:     ApiError.Err,
+		Details: ApiError.Details,
 	})
 }
